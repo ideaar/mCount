6916043a9d62e25f13202ab16734a64dd7416918
idearr <295136728@qq.com>
2016-05-25 19:15:34 +0800

    特定页面隐藏tabs

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,27 +1,12 @@
 angular.module('starter.directives', [])
-
-.directive('hideTabs', function($rootScope) {
+.directive('hideTabs',function($rootScope){
     return {
-        restrict: 'A',
-        link: function(scope, element, attributes) {
-
-            scope.$on('$ionicView.beforeEnter', function() {
-
-                scope.$watch(attributes.hideTabs, function(value){
-                    $rootScope.hideTabs = 'tabs-item-hide';
-                });
-
-            });
-
-            scope.$on('$ionicView.beforeLeave', function() {
-                scope.$watch(attributes.hideTabs, function(value){
-                    $rootScope.hideTabs = 'tabs-item-hide';
-                });
-                scope.$watch('$destroy',function(){
-                    $rootScope.hideTabs = false;
-                })
-
-            });
+        restrict:'AE',
+        link:function($scope){
+            $rootScope.hideTabs = 'tabs-item-hide';
+            $scope.$on('$destroy',function(){
+                $rootScope.hideTabs = ' ';
+            })
         }
-    };
+    }
 })
